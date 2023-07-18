18cf3985e94b75051b18c7a3d267de7cc13010f5
quang1409thanh <thanhaxt342@gmail.com>
2023-07-19 00:57:45 +0700

    testL1

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -85,7 +85,7 @@ bool checkContinuePlaying(char isContinued)
 {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if (isContinued = 'Y')
+    if (isContinued = 'Y' || isContinued = 'y')
     {
         result = true;
     }
